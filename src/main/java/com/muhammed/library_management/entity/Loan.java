fc4a509607c23a63952722a8ad6a16bcee972c68
muhammedsaldamli <muhammeds.saldamli@gmail.com>
2026-09-06 02:19:18 +0300

    h2 konsol book ve loan yapıları oluşturma

---
+++ src/main/java/com/muhammed/library_management/entity/Loan.java
@@ -0,0 +1,39 @@
+package com.muhammed.library_management.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "loans")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Loan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Column(nullable = false)
+    private LocalDate loanDate;
+
+    @Column(nullable = false)
+    private LocalDate dueDate;
+
+    private LocalDate returnDate; // null ise kitap hala ödünçte demektir
+}
---
+++ src/main/java/com/muhammed/library_management/entity/Member.java
@@ -0,0 +1,26 @@
+package com.muhammed.library_management.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "members")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+}
